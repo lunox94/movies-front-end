430af7d9e9ee20c22bb776e38aefaceaf42f363a
gerardnts <87734279+gerardnts@users.noreply.github.com>
2021-09-12 16:57:43 -0400

    chore: refactor markdown component

--- src/app/shared/input-markdown/input-markdown.component.ts
+++ src/app/shared/input-markdown/input-markdown.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-const PREVIEW_PLACEHOLDER = 'Biography preview';
-
 @Component({
     selector: 'app-input-markdown',
     templateUrl: './input-markdown.component.html',
 })
 export class InputMarkdownComponent implements OnInit {
-    markdown: string = PREVIEW_PLACEHOLDER;
+    markdown?: string;
     constructor() {}
 
     ngOnInit(): void {}
 
     onChanges(event: Event): void {
         const target = event.target as HTMLInputElement;
-        this.markdown = target.value ? target.value : PREVIEW_PLACEHOLDER;
+        this.markdown = target.value;
     }
 }
