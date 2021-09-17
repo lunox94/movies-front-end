b086338dfd3f1539e88d68f606f7de7c91fdf45f
gerardnts <87734279+gerardnts@users.noreply.github.com>
2021-09-17 15:39:30 -0400

    chore: setup cinemas routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,19 +2,24 @@ import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
     {
-        path: 'movies',
-        loadChildren: () =>
-            import('./movies/movies.module').then((m) => m.MoviesModule),
-    },
-    {
         path: 'actors',
         loadChildren: () =>
             import('./actors/actors.module').then((m) => m.ActorsModule),
     },
     {
+        path: 'cinemas',
+        loadChildren: () =>
+            import('./cinemas/cinemas.module').then((m) => m.CinemasModule),
+    },
+    {
         path: 'genres',
         loadChildren: () =>
             import('./genres/genres.module').then((m) => m.GenresModule),
+    },
+    {
+        path: 'movies',
+        loadChildren: () =>
+            import('./movies/movies.module').then((m) => m.MoviesModule),
     },
     {
         path: '**',
