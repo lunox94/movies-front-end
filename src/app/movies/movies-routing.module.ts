eb3e2c173ab009e838dadb3e60c0e32d8432103b
gerardnts <87734279+gerardnts@users.noreply.github.com>
2021-10-01 12:58:21 -0400

    chore: refactor movies routes

--- src/app/movies/movies-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { moviesRoutes } from "./movies.routing";
-
-@NgModule({
-    imports: [RouterModule.forChild(moviesRoutes)],
-    exports: [RouterModule]
-})
-export class MoviesRoutingModule {}
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -5,13 +5,14 @@ import { GenericListModule } from '../shared/generic-list/generic-list.module';
 import { MaterialModule } from '../shared/material/material.module';
 import { RatingModule } from '../shared/rating/rating.module';
 import { ListMoviesComponent } from './pages/list-movies/list-movies.component';
-import { MoviesRoutingModule } from './movies-routing.module';
 import { AddEditMovieFormComponent } from './components/add-edit-movie-form/add-edit-movie-form.component';
 import { AddMovieComponent } from './pages/add-movie/add-movie.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InputMarkdownModule } from '../shared/input-markdown/input-markdown.module';
 import { ImageSelectorModule } from '../shared/image-selector/image-selector.module';
 import { SelectActorsComponent } from './components/select-actors/select-actors.component';
+import { RouterModule } from '@angular/router';
+import { moviesRoutes } from './movies.routing';
 
 @NgModule({
     declarations: [
@@ -22,11 +23,11 @@ import { SelectActorsComponent } from './components/select-actors/select-actors.
         SelectActorsComponent,
     ],
     imports: [
+        RouterModule.forChild(moviesRoutes),
         GenericListModule,
         RatingModule,
         CommonModule,
         ReactiveFormsModule,
-        MoviesRoutingModule,
         InputMarkdownModule,
         ImageSelectorModule,
         MaterialModule,
