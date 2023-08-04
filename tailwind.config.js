f810e25f352ef3a1cd20e569f57aaf32b1f9ca22
lunox94 <gmrlunox94@gmail.com>
2023-08-03 23:06:52 -0400

    chore - update tailwind and fix styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1,
     content: [
-      "./src/**/*.{html,ts}",
-      "./projects/**/*.{html,ts}"
-    ]
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+        "./src/**/*.{html,ts}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
